2079d83a3efeca10ae184323aa38838b57a65f49
DevoMike <Mbonu.mike@yahoo.com>
2023-04-08 14:57:33 +0000

    update on App.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+import sys
 
 from services.home_activities import *
 from services.notifications_activities import *
@@ -13,6 +14,8 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
+
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
 
 #Honeycomb
 from opentelemetry import trace
@@ -30,9 +33,10 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 #cloudwatch logs
 import watchtower
 import logging
-from time import strftime
+
 
 #IMPORT ROLLBAR
+from time import strftime
 import rollbar
 import rollbar.contrib.flask
 from flask import got_request_exception
@@ -64,6 +68,14 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+cognito_jwt_token = CognitoJwtToken(
+  user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
+  user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region=os.getenv("AWS_DEFAULT_REGION")
+)
+
+#x-ray
 XRayMiddleware(app, xray_recorder)
 
 #Honeycomb____
